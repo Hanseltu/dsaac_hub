7c84ca314ec2e87082d2941c28876505c1ee94e2
Hanseltu <tuhaoxin6@gmail.com>
2019-04-02 21:56:51 +0800

    master

---
+++ programs/chapter2/cmake-build-debug/CMakeFiles/chapter2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter2.dir/2.1_sequentialSearch.cpp.o"
+  "chapter2.pdb"
+  "chapter2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chapter2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
